6542e6507fe736135928138ec6a233ea0106cc8d
JaimeWX <1261360793@qq.com>
2023-04-25 16:45:34 +0800

    backward()

--- autograd_backward.py
+++ autograd_backward.py
@@ -2,16 +2,16 @@ import torch
 
 if __name__ == '__main__':
     # retain_graph的使用示例
-    # w = torch.tensor(data=[1.],requires_grad=True)
-    # x = torch.tensor(data=[2.],requires_grad=True)
+    w = torch.tensor(data=[1.],requires_grad=True)
+    x = torch.tensor(data=[2.],requires_grad=True)
 
-    # a = torch.add(w,x)
-    # b = torch.add(w,1)
-    # y = torch.mul(a,b)
+    a = torch.add(w,x)
+    b = torch.add(w,1)
+    y = torch.mul(a,b)
 
-    # y.backward(retain_graph=True)
-    # y.backward()
-    # print(w.grad)
+    y.backward(retain_graph=True)
+    y.backward()
+    print(w.grad)
 
     # grad_tensors的使用示例
     w = torch.tensor(data=[1.],requires_grad=True) 
