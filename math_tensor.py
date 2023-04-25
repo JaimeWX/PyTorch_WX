8da8988496b7eca527719ec3daf14eeb08df4ecd
JaimeWX <1261360793@qq.com>
2023-04-25 11:04:55 +0800

    tensor的数学运算

--- math_tensor.py
+++ math_tensor.py
@@ -1,9 +1,14 @@
 import torch
 
 if __name__ == '__main__':
-    tensor1 = torch.randn(size=(3,3))
-    tensor2 = torch.ones_like(input=tensor1)
-    tensor_add = torch.add(input=tensor1,alpha=10,other=tensor2)
-    print(tensor1)
+    # tensor1 = torch.randn(size=(3,3))
+    # tensor2 = torch.ones_like(input=tensor1)
+    # tensor_add = torch.add(input=tensor1,alpha=10,other=tensor2)
+    # print(tensor1)
+    # print(tensor2)
+    # print(tensor_add)
+
+    torch.manual_seed(10)
+    tensor2 = torch.randint(low=0,high=10,size=(2,3)).float()
     print(tensor2)
-    print(tensor_add)
+    print(tensor2.mean())
